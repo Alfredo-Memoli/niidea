db4a7ee45ac8ffb3984d52c1464d4389c18fd6d1
Alfredo-Memoli <armemoli@itpfp.com>
2023-12-13 14:00:28 +0100

    Inicio

---
+++ src/main/java/com/mycompany/niidea/Niidea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.niidea;
+
+/**
+ *
+ * @author alfre
+ */
+public class Niidea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
